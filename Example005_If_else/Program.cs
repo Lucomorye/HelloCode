2ca1bf2eac1dfb6babc1eda1a509f8ebde378dd5
Tarantin <mr.fadeyka@gmail.com>
2023-03-28 08:27:17 +0300

    add Example 005 if else

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "masha";
+Console.Write("Enter name of user => ");
+string enterName = Console.ReadLine();
+
+if(enterName.ToLower() == name)
+{
+  Console.WriteLine(":):):)");
+}
+else
+{
+  Console.Write("Hi, ");
+  Console.WriteLine(enterName);
+}
